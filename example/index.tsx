d9c5f62f993121bb4d1eebc08951fbc2812c8f18
wuyunqiang <wuyunqiang@kuaishou.com>
2024-09-26 20:38:57 +0800

    feat: s删除部分属性

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,8 @@
 import React, { memo, useEffect, useRef, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import WaterFallList, { type IWaterFallList } from "../src/index";
+import WaterFallList from "../src/index";
 import Item from "./item";
+import { IWaterFallList } from "../src/type";
 
 const getColor = () => {
   const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
--- src/index.tsx
+++ src/index.tsx
@@ -1,73 +1,28 @@
 import React, {
-  type ForwardRefRenderFunction,
-  forwardRef,
-  memo,
-  useEffect,
-  useImperativeHandle,
+  ForwardRefRenderFunction,
   useRef,
   useState,
+  useEffect,
+  useImperativeHandle,
+  memo,
+  forwardRef,
 } from "react";
-import {
-  FlatList,
-  type FlatListProps,
-  View,
-  type ViewStyle,
-} from "react-native";
-
-export type ItemData = {
-  offsetTop: number; // !! 元素距离容器顶部的距离
-  itemH: number; // 元素高度
-  itemData: any; // 元素数据
-  columnIndex: number; // 当前元素所在的列
-  index: number; // 原始列表的位置索引
-};
-
-export type RowData = {
-  rowIndex: number; // 第几行
-  rowData: ItemData[]; // 每行的列数据
-  rowH: number; // 行高度
-  rowOffsetTop: number; // !! 每行距离容器顶部的距离
-};
-
-export type IRenderItemProps = {
-  item: ItemData;
-  index: number;
-  row: RowData;
-};
-
-export interface IWaterFallListProps
-  extends Omit<
-    FlatListProps<RowData>,
-    "renderItem" | "ItemSeparatorComponent"
-  > {
-  renderItem: ({
-    item,
-    index,
-    row,
-  }: IRenderItemProps) => React.ReactElement | null;
-  ItemSeparatorComponent?: () => JSX.Element;
-  children?: React.ReactNode; // 添加children类型
-  rowStyle?: ViewStyle;
-  data: any[];
-}
-
-export interface IWaterFallList {
-  refreshList: (offset?: number, animated?: boolean) => void;
-  flatList: FlatList | null;
-}
+import { FlatList, View } from "react-native";
+import { IWaterFallList, IWaterFallListProps, RowData, ItemData } from "./type";
 
 const WaterFallList: ForwardRefRenderFunction<
   IWaterFallList,
   IWaterFallListProps
 > = (props: IWaterFallListProps, ref) => {
+  const p = { ...props };
+  Reflect.deleteProperty(p, "getItemLayout");
   const {
     data,
     numColumns = 2,
     rowStyle,
-    getItemLayout,
     ItemSeparatorComponent,
     ...otherProps
-  } = props;
+  } = p;
   const _itemHeightsRef = useRef<number[]>([]);
   const flatListRef = useRef<FlatList>(null);
 
